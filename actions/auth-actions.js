8c896d313efdc743bb6f946c3f480051cfd6ce28
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-04-30 03:32:47 +0100

    Initial Commit

---
+++ actions/auth-actions.js
@@ -0,0 +1,42 @@
+"use server";
+
+import { createAuthSession } from "@/lib/auth";
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long.";
+  }
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+
+  const hashedPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect("/product");
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "Email address is already in use.",
+        },
+      };
+    }
+    throw error;
+  }
+}
